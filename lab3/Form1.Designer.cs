9749544f5f30c9c7346dcf45f39bf15d4b9d0c92
Vic Annielyn Coronel <v.coronel.525238@umindanao.edu.ph>
2025-02-09 13:56:46 +0800

    Commit 1

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,79 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(39, 27);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Enter";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += buttonShowInfo_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 19;
+            listBox1.Location = new Point(166, 88);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(587, 327);
+            listBox1.TabIndex = 2;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(166, 29);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(593, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(39, 97);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 3;
+            button2.Text = "Enter";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += buttonShowBookList_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(listBox1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button button1;
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button button2;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,90 @@
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Book
+        {
+            public string? Title { get; set; }
+            public string? Author { get; set; }
+            public virtual string GetInfo()
+            {
+                return $"{Title} by {Author}";
+            }
+        }
+        public class Magazine : Book
+        {
+            public int IssueNumber { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title}\t\tby {Author}\t   Issue: {IssueNumber}\n";
+
+            }
+        }
+        public class Ebook : Book
+        {
+            public string? Platform { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title}\tby {Author}\t   Platform: {Platform}\n";
+            }
+        }
+        public class TextBook : Book
+        {
+            public string? Subject { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title}\t\tby {Author}\t   Subject: {Subject}\n";
+            }
+        }
+        public class Audiobook : Book
+        {
+            public string? Narrator { get; set; }
+            public int Duration { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title}\t\tby: {Narrator}\t   Duration: {Duration} minutes\n";
+            }
+        }
+
+        private void buttonShowInfo_Click(object sender, EventArgs e)
+        {
+            Book myBook = new Magazine()
+            {
+                Title = "Tech Weekly",
+                Author = "John Doe",
+                IssueNumber = 45
+            };
+            MessageBox.Show(myBook.GetInfo());
+        }
+        private void buttonShowBookList_Click(object sender, EventArgs e)
+        {
+            Book[] books = {
+               new Magazine() { Title = "Sports Illustrated", Author = "Editorial Board", IssueNumber = 98 },
+                new Ebook() { Title = "Digital Marketing 101", Author = "Lisa Green", Platform = "Google Books" },
+                new TextBook() { Title = "Software Design", Author = "Andrew Bell", Subject = "Computer Science" },
+                new Audiobook() { Title = "The Art of War", Author = "Sun Tzu", Narrator = "Morgan Freeman", Duration = 200 }
+            };
+
+            listBox1.Items.Clear();
+            foreach (var book in books)
+            {
+                listBox1.Items.Add(book.GetInfo());
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
 }
